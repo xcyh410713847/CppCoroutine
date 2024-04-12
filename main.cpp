913eea8849bfd04e0e35a26af5ed8629b0323121
Shyfan <410713847@qq.com>
2024-04-12 11:27:35 +0800

    初始

---
+++ main.cpp
@@ -0,0 +1,20 @@
+﻿/*************************************************************************
+**文件: main.cpp
+**作者: shyfan
+**日期: 2024/04/12 11:20:04
+**功能: 主函数
+*************************************************************************/
+
+#include <iostream>
+
+#include "src/Coroutine.h"
+
+int main()
+{
+    MyFuture<int> result = async_wait(2); // 启动协程，等待2秒
+    std::cout << "Waiting..." << std::endl;
+    // 获取协程的结果
+    std::cout << "Result: " << result.get() << std::endl;
+
+    return 0;
+}
