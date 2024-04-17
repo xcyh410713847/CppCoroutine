7ab3422edbdf0b06777f53ed409ee81ea5ee9fc7
Shyfan <410713847@qq.com>
2024-04-17 20:14:46 +0800

    保存

--- SkywalkerCoroutine/SkywalkerCoroutine.h
+++ SkywalkerCoroutine/SkywalkerCoroutine.h
@@ -32,7 +32,6 @@
     };                                                               \
     struct CoroutineName::promise_type                               \
     {                                                                \
-        int value;                                                   \
         std::suspend_always initial_suspend() { return {}; }         \
         std::suspend_always final_suspend() noexcept { return {}; }  \
         auto get_return_object()                                     \
@@ -49,53 +48,3 @@
     {                                                                \
         co_return;                                                   \
     }
-
-// 协程的返回对象
-template <typename T>
-struct MyFuture
-{
-    struct promise_type;
-    using handle_type = std::coroutine_handle<promise_type>;
-
-    MyFuture(handle_type h) : coro_handle(h) {}
-    ~MyFuture()
-    {
-        if (coro_handle)
-            coro_handle.destroy();
-    }
-
-    T get()
-    { // 获取异步结果
-        if (coro_handle)
-        {
-            coro_handle.resume();
-        }
-        return coro_handle.promise().value;
-    }
-
-    handle_type coro_handle;
-};
-
-template <typename T>
-struct MyFuture<T>::promise_type
-{
-    T value;
-    std::suspend_always initial_suspend() { return {}; }
-    std::suspend_always final_suspend() noexcept { return {}; }
-    auto get_return_object()
-    {
-        return MyFuture<T>{handle_type::from_promise(*this)};
-    }
-    void return_value(T v) { value = v; } // 设置协程返回值
-    void unhandled_exception()
-    {
-        std::exit(1); // 简单处理异常：直接退出程序
-    }
-};
-
-// 异步等待一段时间
-MyFuture<int> Create(int seconds)
-{
-    // 模拟异步操作，简单地等一段时间
-    co_return 42; // 协程最终返回的结果
-}
--- main.cpp
+++ main.cpp
@@ -13,11 +13,6 @@ SkywalkerCo_Create(SkywalkerCoTest);
 
 int main()
 {
-    MyFuture<int> result = Create(2); // 启动协程，等待2秒
-    std::cout << "Waiting..." << std::endl;
-    // 获取协程的结果
-    std::cout << "Result: " << result.get() << std::endl;
-
     SkywalkerCoTest co = SkywalkerCoTestCreate();
     co.get();
 
